c6d3468534a0a70b1fc3e1f2760a032f75d28f26
jordan <sorgel_jordan_leigh@student.smc.edu>
2020-10-19 19:34:56 -0400

    Slider
    Changed pepper images in slider
    added js for slider

--- midterm/script.js
+++ midterm/script.js
@@ -1,12 +1,46 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
 
-  // NEWSLETTER MODAL  
-  $("#newsletterModal").modal('show');
-
-
-
+// NEWSLETTER MODAL  
+$("#newsletterModal").modal('show');
 
 
 
+
+
+// pause/ play button on carousel
+
+$('.carousel').carousel({
+  interval: 3000,
+  keyboard: false
 });
+
+let carouselPaused = false;
+
+$('.js-carousel-pause').click(function () {
+  if (carouselPaused) {
+    $('.carousel').carousel('cycle');
+    $(this).text("Select");
+  }
+  else {
+    $('.carousel').carousel('pause');
+    $(this).text("Unselect");
+  }
+
+  carouselPaused = !carouselPaused;
+});
+
+$('.js-modal-launch').click(function () {
+  $('#myModal').modal({
+    backdrop: 'static'
+  });
+})
+
+
+
+
+
+
+
+
+// });
