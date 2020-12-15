0a2954d397efb0d1d50a094faa6b6052e6c56b05
jordan <sorgel_jordan_leigh@student.smc.edu>
2020-12-14 20:00:15 -0500

    Added cart page
    cart page with checkout form & JS

--- final/2.js
+++ final/2.js
@@ -24,17 +24,59 @@ $(document).ready(function () {
   });
 
 
-  // cart!
-
-
-
-
+  // cart numbers!
 
   $('.add-cart').click(function () {
     $('.count').html(function (i, val) {
       return val * 1 + 1
     });
   });
+
+
+  // checkout page!
+
+
+
+
+
+  $("#checkOut").click(function () {
+    $("#checkoutForm").show();
+  });
+
+
+  function Fillerup() {
+
+    if (document.getElementById('same-address').checked) {
+
+      document.getElementById('firstNameShipping').value = document.getElementById('firstName').value;
+
+      document.getElementById('lastNameShipping').value = document.getElementById('lastName').value;
+      document.getElementById('addressShipping').value = document.getElementById('address').value;
+      document.getElementById('address2Shipping').value = document.getElementById('address2').value;
+      document.getElementById('zipShipping').value = document.getElementById('zip').value;
+      document.getElementById('cityShipping').value = document.getElementById('city').value;
+      document.getElementById('stateShipping').value = document.getElementById('state').value;
+    } else {
+      document.getElementById('firstNameShipping').value = "";
+      document.getElementById('lastNameShipping').value = "";
+      document.getElementById('addressShipping').value = "";
+      document.getElementById('address2Shipping').value = "";
+      document.getElementById('zipShipping').value = "";
+      document.getElementById('cityShipping').value = "";
+      document.getElementById('stateShipping').value = "";
+    }
+  }
+
+
+
+  let form = document.getElementById('myForm');
+  form.addEventListener("submit", function (evt) {
+    if (form.checkValidity() === false) {
+      evt.preventDefault();
+      evt.stopPropagation();
+    }
+    form.classList.add('was-validated');
+  })
 
 
 
